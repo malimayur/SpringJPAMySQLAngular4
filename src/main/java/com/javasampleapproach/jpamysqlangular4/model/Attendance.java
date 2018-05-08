e6426727c319ae9f5abc0df1ee476d946cfd5d42
Mayurkumar Mali <MM00479186@INPUHJPC10672.TechMahindra.com>
2018-05-08 11:54:50 +0530

    Added Attendance model

---
+++ src/main/java/com/javasampleapproach/jpamysqlangular4/model/Attendance.java
@@ -0,0 +1,68 @@
+package com.javasampleapproach.jpamysqlangular4.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "attendance")
+public class Attendance implements Serializable {
+
+	private static final long serialVersionUID = -3009157732242241604L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+    private Student student;
+	
+	@Column(name = "intime")
+	private long inTime;
+	
+	@Column(name = "outtime")
+	private long outTime;
+	
+	public Attendance() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public long getInTime() {
+		return inTime;
+	}
+
+	public void setInTime(long inTime) {
+		this.inTime = inTime;
+	}
+
+	public long getOutTime() {
+		return outTime;
+	}
+
+	public void setOutTime(long outTime) {
+		this.outTime = outTime;
+	}
+	
+}
